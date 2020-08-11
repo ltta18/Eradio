7f1f34d03a35838a58cde29f1533a6cb91cb7300
tk14shiina <49159641+tk14shiina@users.noreply.github.com>
2020-08-11 16:38:18 +0700

    Update link api

--- src/api/Action/User/AuthAction.js
+++ src/api/Action/User/AuthAction.js
@@ -70,7 +70,7 @@ export const fetchLogOut = (token) => (dispatch) => {
   try {
     const response = axios({
       method: 'POST',
-      url: 'https://10.2.50.232:1209/auth/logout',
+      url: 'http://10.2.50.232:1209/auth/logout',
       headers: {
         Authorization: `Bearer ${token}`
       },
