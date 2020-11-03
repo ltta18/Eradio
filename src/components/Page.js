46dd514ba06ec5010a4b15603bcf024b5479028e
tk14shiina <49159641+tk14shiina@users.noreply.github.com>
2020-11-03 14:28:00 +0700

    Update token

--- src/components/Page.js
+++ src/components/Page.js
@@ -20,7 +20,7 @@ const Page = () => {
   const dispatch = useDispatch()
 
   // const token = useSelector(selectAccessToken)
-  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDQwNTUwMzYsImlhdCI6MTYwNDA1MTQzMSwic3ViIjoyfQ.oFs49DjykyDmIjvsKAYpxPW9GxxfbnWPcn7CGTepS2Y'
+  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDk1NzUxMjYsImlhdCI6MTYwNDM4NzUyMSwic3ViIjoyfQ.OOg3c0H-XDQQ7F3lQ9r6HFkB-P2UrrvG20W41EMzdZU'
   const [ name, setName ] = useState('')
   
   useEffect(() => {
